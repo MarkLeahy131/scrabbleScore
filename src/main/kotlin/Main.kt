b4c8f4711582217220faf89f26aebf9205913706
MarkLeahy2 <82410431+MarkLeahy2@users.noreply.github.com>
2022-02-19 20:50:53 +0000

    Tested code. "Mark" returned a value of 10 which is correct

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,6 +15,7 @@ fun main (){//(args: Array<String>) {
     while (true) {
         println("Enter a word and compute the Scrabble Score: ") //added println
         val word = input.nextLine()
+
         //TODO change to use a string template:
         println(word + "has a value of" + scrabbleScore.scoreWord(word))
     }
